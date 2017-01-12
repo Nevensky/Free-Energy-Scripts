0bce2bd6704162716f69b2a805b3d82e6ad46308
VPFK Neven G <sutods@vpfk>
2017-01-12 15:05:55 +0100

    Dodane postojeće skripte.

---
+++ BAR/apsolutne/gen_barinput.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+ 
+# skripta za generiranje Free Energy BAR inputa
+ 
+import sys, getopt
+import click
+from colorama import Fore,Style
+import os.path
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+import re 
+ 
+unil='\u03BB'
+ 
+@click.command()
+
+@click.option('--nsim',default=20,help='Total number of simulations.')
+@click.option('--vdw',default=False,help='Mutate van der Waals interactions.')
+@click.option('--coul',default=False,help='Mutate Coulomb interactions.')
+@click.option('--mass',default=False,help='Mutate mass of species.')
+@click.option('--bonded',default=False,help='Mutate bonded interactions.')
+@click.option('--restraint',default=False,help='Mutate restrained interactions.')
+@click.option('--temp',default=False,help='Simulated temptering.')
+
+@click.option('-mdp',default='./MDP',help='MDP folder.')
+@click.option('-min1',default='./MDP/EM/em_steep.mdp',help='MDP of steepest decent minimization.')
+@click.option('-min2',default='./MDP/EM/em_l-bfgs.mdp',help='MDP of L-BFGS minimization.')
+@click.option('-nvt',default='./MDP/NVT/nvt.mdp',help='MDP of NVT equilibration.')
+@click.option('-npt',default='./MDP/NPT/npt.mdp',help='MDP of NPT equilibration.')
+@click.option('-prod',default='./MDP/Production_MD/md.mdp',help='MDP of the production run.')
+@click.option('-ncores',default=4,help='Number of cores per simulation.')
+@click.option('-pin',default='auto',help='Pin to cores.')
+ 
+def inputs(nsim,vdw,coul,mass,bonded,restraint,temp,mdp,min1,min2,nvt,npt,prod,ncores,pin):
+	'''Free Energy BAR input generator. \n
+
+Developed by Neven Golenic | neven.golenic@gmail.com
+
+\b
+Number of simulations [ N ] default: [ 20 ]
+\u03BB parameters [ N_min,N_max ]
+
+\b
+Recommended inital folder structure:
+.
+└── MDP
+|   |
+|   ├─ EM
+|   |  ├── em_steep.mdp
+|   |  └── em_l-bfgs.mdp
+|   ├─ NVT
+|   |   └── nvt.mdp 
+|   ├─ NPT
+|   |   └── npt.mdp 
+|   └─ Production_MD
+|       └── md.mdp
+|
+└── STRUCT
+      |
+      ├─ system.top
+      └─ system.gro
+'''
+	print('')
+#    bonded=bonded.split(',')
+#    restraint=restraint.split(',')
+#    temp=temp.split(',')
+	check_mdp(mdp,min1,min2,nvt,npt,prod)
+
+	if vdw!=False:
+		vdw=vdw.split(',')
+		vdwstr='Coupling van der Waals interactions from '+Fore.MAGENTA+unil+'({0})'+Fore.RESET+' to '+Fore.MAGENTA+unil+'({1})'+Fore.RESET
+		print(vdwstr.format(vdw[0],vdw[1]))
+		vdw_lambdas = 'vdw_lambdas = '+" ".join(create_lambdas(nsim,int(vdw[0]),int(vdw[1])))+'\n'
+		print(5*'-----','\n',vdw_lambdas,5*'______','\n')
+		import_mdp(min1,min2,nvt,npt,prod,vdw_lambdas)
+	if coul!=False:
+		coul=coul.split(',')
+		coulstr='Coupling Coulomb interactions from '+Fore.MAGENTA+unil+'({0})'+Fore.RESET+' to '+Fore.MAGENTA+unil+'({1})'+Fore.RESET
+		print(coulstr.format(coul[0],coul[1]))
+		coul_lambdas='coul_lambdas = '+" ".join(create_lambdas(nsim,int(coul[0]),int(coul[1])))
+		print(5*'-----','\n',coul_lambdas,'\n',5*'______','\n')
+	if mass!=False:
+		mass=mass.split(',')
+		massstr='Coupling mass lambdas from '+Fore.MAGENTA+unil+'({0})'+Fore.RESET+' to '+Fore.MAGENTA+unil+'({1})'+Fore.RESET
+		print(massstr.format(mass[0],mass[1]))
+		mass_lambdas='mass_lambdas = '+" ".join(create_lambdas(nsim,int(mass[0]),int(mass[1])))
+		print(5*'-----','\n',mass_lambdas,'\n',5*'______','\n')
+	
+	return None
+ 
+# click.echo(vdw)
+
+# def import_mdp(min1,min2,nvt,npt,prod):
+#     if os.path.isdir("./MDP"):
+#         with open(min1) as file:
+#             min1_lines=[]
+#                 for line in file:
+#                     min1_lines.append(line.strip().split(','))
+#                     if line =="":
+#     return None
+
+def check_mdp(mdp,min1,min2,nvt,npt,prod):
+	# Check if MDP file exists or if all .mdp files were specified manually.
+	if os.path.exists(min1) and os.path.exists(min2) and os.path.exists(nvt) and os.path.exists(npt) and os.path.exists(prod):
+		pass	
+	elif os.path.isdir(mdp) :
+		if os.path.exists(mdp+'/EM/em_steep.mdp') and os.path.exists(mdp+'/EM/em_l-bfgs.mdp') and os.path.exists(mdp+'/NVT/nvt.mdp')==True and os.path.exists(mdp+'/NPT/npt.mdp') and os.path.exists(mdp+'/Production_MD/md.mdp'):
+			pass
+		else: 
+			print(Fore.RED,'ERROR: MDP folder corrupt. Expected folder structure:',Fore.YELLOW,"""\n.
+└── MDP
+    |
+    ├─ EM
+    |  ├── em_steep.mdp
+    |  └── em_l-bfgs.mdp
+    ├─ NVT
+    |   └── nvt.mdp 
+    ├─ NPT
+    |   └── npt.mdp 
+    └─ Production_MD
+        └── md.mdp""",Fore.RESET)
+			exit()
+		#print("MDP exists!")
+	else:
+		print(Fore.RED,"ERROR: MDP folder not found, please specify paths to ALL .mdp files manually.",Fore.RESET)
+		exit()
+
+def import_mdp(min1,min2,nvt,npt,prod,vdw_lambdas):
+	# Imports mdp files and replaces them with new ones with correct lambdas
+	with open(min1) as file:
+		filedata = open('MDP/em_steep.mdp','w')
+		for line in file:
+			#filedata = file.read(line.replace('vdw_lambdas',vdw_lambdas))
+			if "vdw_lambdas" in line:
+				filedata.write(vdw_lambdas)
+			else:
+				filedata.write(line)
+		filedata.close()
+	#    filedata = filedata.replace('vdw_lambdas', vdw_lambdas)
+	# Write the file out again
+#    with open('file.txt', 'w') as file:
+#        file.write(filedata)
+
+def write_mdp():
+	return None
+
+def gen_mdp():
+	return None
+#ncpus = 1
+#nsim = 20
+ 
+def create_lambdas(nsim,start,end):
+	# Creates lambda values equidistant with respect to lambdas
+	lambdas = []
+	lambda_0 = 1.0/(end-start)
+	for i in range(nsim+1):
+		lambda_i=lambda_0*(i-start)
+		if i>=start and i<=end:
+			lambdas.append("{0:.4f}".format(lambda_i))
+		else:
+			lambdas.append("0.0000")
+	return lambdas
+ 
+def import_bar(bar):
+	ddG = []
+	with open(bar,'r') as file:
+		i = 0
+		for line in file:
+			if 'Final results in kJ/mol:' in line:
+			   i = 1
+			elif i==1:
+			   ddG.append(re.sub("\s\s+", " ", line).split(' '))
+	np.loadtxt(barfile,usecols=(1,5))
+	
+	return 
+
+def create_lambdas_equiG(nsim,start,end):
+	# Creates lambda values equidistant with respect to DDG
+	interp1d()
+
+ 
+if __name__ == '__main__':
+	inputs()
---
+++ Umbrella Sampling/plot_distances.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+#skripta za plotanje udaljenosti i generiranje optimalnog razmaka konfiguracija
+# generira pocetni email report u analysis/email_report.txt
+
+import matplotlib
+matplotlib.use('PDF')
+from matplotlib import pyplot as plt
+import numpy as np
+import click
+import os #to get workign dir
+import socket #get computer name
+
+#max_dist=1.1 # nm
+#min_sep=0.2 # nm
+
+@click.command()
+
+@click.option('-max_dist',default='1',help='Maximum distance [nm] of coord solvent from starting position.')
+@click.option('-min_dist',default=False,help='Minimum distance [nm] of coord solvent from starting position.')
+@click.option('-min_sep',default='0.2',help='Minimum sepearation distance [nm] between subsequent windows.')
+@click.option('-omit',default='0.0',help='Omit all confs up to defined number.')
+@click.option('-umbrella_dir',default=os.getcwd(),help='Path to simulation root dir.')
+@click.option('-nskip',default=1,help='How many frames were skipped in trjconv.')
+
+def inputs(max_dist,min_dist,min_sep,umbrella_dir,nskip):
+	'''Umbrella sampling mid-step. Plot distances between coord solv and ligand for all generated configurations and select meaningful ones based on 
+	maximum distance and window sepearation distance. \n
+
+Developed by Neven Golenic | neven.golenic@gmail.com'''
+	
+	path = umbrella_dir+"/CONF"
+	filenames = [item for item in next(os.walk(path))[2] if ".gro" in item]
+	num_files = len(filenames)-1 # ide od 0 - 5000 .... jedna viska za plotanje
+
+	max_dist=float(max_dist)
+	min_dist=float(min_dist)
+	min_sep=float(min_sep)
+	omit=int(omit)
+	data = np.genfromtxt(umbrella_dir+'/summary_distances.dat', delimiter = '\t',skip_header=1)
+
+	order = data[omit:, 1].argsort()
+	sorted_data = np.take(data, order, 0)
+
+	desired_confs=["0 \n"]
+	desired_confs_vis=[umbrella_dir+"/CONF/conf0.gro "]
+#	desired_confs_value[sorted_data[0,:]]
+	print(sorted_data[:,1])
+	
+	track_k=0
+	if min_dist==False:
+		z=sorted_data[0,1]
+	else:
+		for v in range(len(sorted_data[:,0])):
+			if sorted_data[v,1]>=min_dist and track_k==0:
+				z=sorted_data[v,1]
+				track_k=v
+				print(track_k)
+			else:
+				pass
+
+	desired_confs_values=[]
+	for k in range(len(sorted_data[:,0])-1):
+		if k>=track_k:
+			if np.isnan(sorted_data[k+1,1])!=True:
+				if np.abs(sorted_data[k+1,1]-z)>min_sep and sorted_data[k+1,1]<max_dist:
+					#print(k+1,np.abs(sorted_data[k+1,1]-z))
+					print(sorted_data[k+1,:])
+					#print(np.abs(sorted_data[k+1,1]),"    ",k+1)
+					z=sorted_data[k+1,1]
+					desired_confs.append(str(k+1)+"\n")
+					desired_confs_vis.append(umbrella_dir+"/CONF/conf"+str(k+1)+".gro ")
+					desired_confs_values.append(np.ndarray.tolist(sorted_data[k+1,:]))
+	#desired_confs_values=[str(k) for k in desired_confs_values]
+	print("maximum distance from initial positon = ",max_dist,"[nm]")
+	print("minimum separation between windows = ",min_sep,"[nm]")
+	print("number of windows = ",len(desired_confs))
+	print("list of window confs = ",[int(l) for l in desired_confs])
+	print("### Seleted confs ###\n",6*"t[ps] \t d[nm] \t ","\n",fmtcols(desired_confs_values,6))
+
+#################################################################################
+############################ EMAIL REPORT #######################################
+	top_info="simulation title = \t"
+	with open("system.top","r") as f3:
+		topology = f3.readlines()
+		for i,line in enumerate(topology):
+			#print(i,line)
+			if "[ system ]" in line:
+				#print(i,line)
+				top_info+=topology[i+1]
+
+
+	with open("analysis/email_report.txt","w") as f3:
+		f3.write(top_info)
+		f3.write("directory = \t"+umbrella_dir+" \n")
+		f3.write("computer name = \t"+socket.gethostname()+" \n")
+		f3.write("Umbrella sampling:"+" \n")
+		f3.write("maximum distance from initial positon = \t"+str(max_dist)+" [nm] \n")
+		f3.write("minimum separation between windows = \t"+str(min_sep)+" [nm] \n")
+		f3.write("number of windows = \t"+str(len(desired_confs))+"\n")
+		f3.write("list of window confs = \t"+str([int(l) for l in desired_confs])+"\n")
+#################################################################################
+
+	# for k in range(len(data[:,0])-1):
+	# 	z=data[0,1]
+	# 	print(np.abs(data[k+1,1]-z)>0.2)
+	# 	if np.abs(data[k+1,1]-z)>0.2:
+	# 		z=data[k+1,1]
+			#print(k,data[k,1])
+
+	with open("confs.txt","w") as f2:
+		f2.writelines(desired_confs)
+		# *10 jer sum dist ispisuje u ps umjesto u broju conf...treba popraviti
+		#f2.writelines([str(int(k)*10)+"\n" for k in desired_confs])
+
+	with open("vmd_visconfs.sh","w") as f2:
+		f2.write("vmd ")
+		f2.writelines(desired_confs_vis)
+
+# scale t[ps] |--> t[conf]
+	data_conf_ratio = data[-1,0]/num_files
+	print(num_files)
+	print(data[-1,0])
+	print(data_conf_ratio)
+
+
+#Pr. data[:,0]*10 jer x[ps] |--> x[conf] simulacija traje 500ps ali ispisujemo 5000 frameova
+	plt.plot(data[:,0]/data_conf_ratio,data[:,1],"r-")
+	for c in desired_confs:
+		# *10 jer sum dist ispisuje u ps umjesto u broju conf...treba popraviti
+		plt.axvline(float(c), color='b', linestyle='-',linewidth="0.05")
+	plt.text(4, 3.8, "max_dist"+str(max_dist)+"nm"+"    min_sep"+str(min_sep))
+	plt.xlabel("conf [number]")
+	plt.ylabel("distance [nm]")
+	plt.title("Distance between the ligand and coordinated solvent")
+	plt.savefig("analysis/pull_distances.pdf")
+
+
+def auto_omit():
+	'''
+	Fit sigmoid to data and determine up to which conf to omit all data.
+	'''
+	pass
+
+def fmtcols(mylist, cols):
+	'''
+	Format columns.
+	'''
+    lines = ("".join(str(mylist[i:i+cols])) for i in range(0,len(mylist),cols))
+    return '\n'.join(lines)
+
+if __name__ == '__main__':
+	inputs()
+
