11e8db4d64e0025055f5e339a68e96ef2b0a1aa9
Nevensky <neven.golenic@gmail.com>
2017-01-23 16:29:41 +0100

    Minor cleanup.

--- BAR/bar_generator/setup.py
+++ BAR/bar_generator/setup.py
@@ -3,6 +3,13 @@
 import os
 from subprocess import call
 
+print("Installing dependencies...")
+call(["conda","install","numpy"])
+call(["pip","install","termcolor"])
+call(["conda","install","scipy"])
+call(["conda","install","matplotlib"])
+call(["conda","install","click"])
+
 call(["chmod","+x",os.getcwd()+"/bar_main.py"])
 call(["rm","-rf",os.getcwd()+"/bin/genbar"])
 call(["ln","-s",os.getcwd()+"/bar_main.py",os.getcwd()+"/bin/genbar"])
@@ -10,3 +17,5 @@ call(["ln","-s",os.getcwd()+"/bar_main.py",os.getcwd()+"/bin/genbar"])
 home_dir = os.getenv("HOME")
 with open(home_dir+"/.bashrc","a") as f:
   f.write("\n#BAR automation script\n"+"export PATH=$PATH:"+os.getcwd()+"/bin/genbar")
+
+print("Command genbar added to .bashrc\n Instalation successful.")
